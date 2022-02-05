99ec404ec553d9c8126c71526c3c3dc1766a26ee
Kesyza <kesyza_210@smkassalaambandung.sch.id>
2022-02-05 11:07:21 +0700

    setengah jalan

--- lib/home.dart
+++ lib/home.dart
@@ -1,25 +1,25 @@
-import 'package:flutter/material.dart';
-import 'package:sholat_sunah/tata.dart';
+// import 'package:flutter/material.dart';
+// import 'package:sholat_sunah/tata.dart';
 
-class HalamanHome extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Beranda'),
-      ),
-      body: Center(
-        child: ListView(
-          children: <Widget>[
-            RaisedButton(
-              child: Text('Sholat Sunnah'),
-              onPressed: () {
-                Navigator.pushNamed(context, TataCara.routeName);
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// class HalamanHome extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Beranda'),
+//       ),
+//       body: Center(
+//         child: ListView(
+//           children: <Widget>[
+//             RaisedButton(
+//               child: Text('Sholat Sunnah'),
+//               onPressed: () {
+//                 Navigator.pushNamed(context, TataCara.routeName);
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,87 @@
 import 'package:flutter/material.dart';
-import 'package:sholat_sunah/home.dart';
+import 'package:sholat_sunah/jenazah.dart';
+import 'package:sholat_sunah/tahajud.dart';
+// import 'package:sholat_sunah/home.dart';
 import 'package:sholat_sunah/tata.dart';
 import 'package:sholat_sunah/duha.dart';
-
-void main() {
-  runApp(SunahDuha());
-}
+import 'package:sholat_sunah/tahajud.dart';
+import 'package:sholat_sunah/tahajud.dart';
+import 'package:sholat_sunah/taubat.dart';
+import 'package:sholat_sunah/witir.dart';
 
 // void main() {
-//   runApp(MaterialApp(
-//     debugShowCheckedModeBanner: false,
-//     title: 'Routing Navigation',
-//     initialRoute: '/',
-//     routes: {
-//       '/': (context) => HalamanHome(),
-//       TataCara.routeName: (context) => TataCara(),
-//     },
-//   ));
+//   runApp(const SunnahTaubat());
 // }
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'sholat_sunah',
+    initialRoute: '/',
+    routes: {
+      '/': (context) => HalamanHome(),
+      SunnahDuha.routeName: (context) => SunnahDuha(),
+      SunnahTahajjud.routeName: (context) => SunnahTahajjud(),
+      SunnahJenazah.routeName: (context) => SunnahJenazah(),
+      SunnahTaubat.routeName: (context) => SunnahTaubat(),
+      SunnahWitir.routeName: (context) => SunnahWitir(),
+    },
+  ));
+}
+
+// halaman home
+class HalamanHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Beranda'),
+      ),
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10),
+              alignment: Alignment.bottomCenter,
+              child: Text("Kumpulan Tata Cara Sholat Sunnah",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.bold)),
+            ),
+            ElevatedButton(
+              child: Text('Sholat Sunnah Dhuha'),
+              onPressed: () {
+                Navigator.pushNamed(context, SunnahDuha.routeName);
+              },
+            ),
+            ElevatedButton(
+              child: Text('Sholat Sunnah Tahajud'),
+              onPressed: () {
+                Navigator.pushNamed(context, SunnahTahajjud.routeName);
+              },
+            ),
+            ElevatedButton(
+              child: Text('Sholat Sunnah Jenazah'),
+              onPressed: () {
+                Navigator.pushNamed(context, SunnahJenazah.routeName);
+              },
+            ),
+            ElevatedButton(
+              child: Text('Sholat Sunnah Taubat'),
+              onPressed: () {
+                Navigator.pushNamed(context, SunnahTaubat.routeName);
+              },
+            ),
+            ElevatedButton(
+              child: Text('Sholat Sunnah Witir'),
+              onPressed: () {
+                Navigator.pushNamed(context, SunnahWitir.routeName);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
